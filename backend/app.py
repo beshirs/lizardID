187684a5cdf2916ec2528e06d2a1dbd12ebf5aa9
Your Name <your.email@example.com>
2025-06-16 15:51:03 -0500

    backend running and flask working

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
-from flask import flask
+from flask import Flask
 from flask_pymongo import PyMongo
+from config import MONGO_URI
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+        return "Backend running"
 
 if __name__ == '__main__':
     app.run(debug=True)
